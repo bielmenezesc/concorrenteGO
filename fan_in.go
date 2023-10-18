610b1f917d152dadac3d3b53952a462dd6976bfc
119110372 GABRIEL MENEZES CABRAL <gabriel.cabral@lcc2-52.lcc.ufcg.edu.br>
2023-10-18 15:55:10 -0300

    atua

--- fan_in.go
+++ fan_in.go
@@ -2,49 +2,48 @@ package main
 
 import (
 	"fmt"
-	"math/rand"
 	"time"
 )
 
 func request_stream() chan string {
 	canal_strings := make(chan string)
-	
-	rand.Seed(time.Now().UnixNano())
-
-	charset := "abcdefghijklmnopqrstuvwxyz"
 
 	go func() {
 		defer close(canal_strings)
 
 		for i := 0; i < 100; i++ {
 
-			randomStr := charset[rand.Intn(len(charset))]
-			canal_strings <- string(randomStr)
+			canal_strings <- "abacate"
 		}
 	}()
 
 	return canal_strings
 }
 
-func ingest(in chan string, ch1 chan string, ch2 chan string) {
+func junta(ch3 chan string, ch1 chan string, ch2 chan string) {
 	for {
 		select {
 		case word := <-ch1:
-			in <- word
-		case word := <-ch2:
-			in <- word
+			ch3 <- word
+		case b := <-ch2:
+			ch3 <- b
 		}
 	}
+}
+
+func ingest(in chan string) {
+	fmt.Println(<-in)
+	time.Sleep(2 * time.Second)
 }
 
 func main() {
 	ch1 := request_stream()
 	ch2 := request_stream()
 	ch3 := make(chan string)
-	
-	go ingest(ch3, ch1, ch2)
 
-	for word := range ch3 {
-		fmt.Println(word)
-	}
+	go ingest(ch3)
+	go junta(ch3, ch1, ch2)
+
+	ch4 := make(chan string)
+	<-ch4
 }
